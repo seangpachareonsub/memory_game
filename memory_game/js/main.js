945089da61d55712bfa87f0d0fb60b73d1e1ff8f
Kenn Seangpachareonsub <seangpachareonsub@Kenns-MacBook-Pro.local>
2020-01-21 20:31:57 +0000

    added 1st stage of js code

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,20 @@
-var cardOne, cardTwo, cardThree, cardFour;
-cardOne = 'queen'
-cardTwo = 'queen'
-cardThree = 'king'
-cardFour = 'king'
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-console.log('User flipped ' + cardOne)
-console.log('User flipped ' + cardThree)
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log('User flipped ' + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log('User flipped ' + cardTwo);
+
+if(cardsInPlay.length === 2) {
+    if(cardsInPlay[0] === cardsInPlay[1]) {
+        alert('You found a match');
+    } else {
+        alert('Sorry, try again');
+    }
+}
