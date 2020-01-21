f6beb010176f3526b58356e3a0c7c7252ba92276
Kenn Seangpachareonsub <seangpachareonsub@Kenns-MacBook-Pro.local>
2020-01-21 20:01:39 +0000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+var cardOne, cardTwo, cardThree, cardFour;
+cardOne = 'queen'
+cardTwo = 'queen'
+cardThree = 'king'
+cardFour = 'king'
+
+console.log('User flipped ' + cardOne)
+console.log('User flipped ' + cardThree)
