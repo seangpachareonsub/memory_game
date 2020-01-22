afc0ac65feb6668c188181936513d77740b478b2
Kenn Seangpachareonsub <seangpachareonsub@Kenns-MacBook-Pro.local>
2020-01-22 16:25:05 +0000

    final version of GA memory game project

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,18 +22,25 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+var clickedImage;
 
-function checkForMatch() {
+function checkForMatch(e) {
+    var cardNumber = clickedImage.getAttribute('data-id');
+    clickedImage.setAttribute('src', cards[cardNumber].cardImage);
     if(cardsInPlay.length === 2) {
         if(cardsInPlay[0] === cardsInPlay[1]) {
           alert('You found a match');
+          location.reload();
       } else {
           alert('Sorry, try again');
+          location.reload();
       }
   }
 }
 
-function flipCard(cardId) {
+function flipCard(e) {
+    var cardId = e.target.getAttribute('data-id');
+    clickedImage = e.target
     console.log('User flipped ' + cards[cardId].rank);
     console.log(cards[cardId].cardImage)
     console.log(cards[cardId].suit)
@@ -41,9 +48,18 @@ function flipCard(cardId) {
     cardsInPlay.push(cards[cardId].rank)
 
     checkForMatch();
-    
 }
 
-flipCard(0);
-flipCard(2);
+//creating 4 images of back of card inside of the div element in the index.html
+function createBoard() {
+    for (let i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src','images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
 
+    }
+}
+
+createBoard();
