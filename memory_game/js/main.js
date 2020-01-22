23510fa81da928624a0cabdad9159b8171b512d2
Kenn Seangpachareonsub <seangpachareonsub@Kenns-MacBook-Pro.local>
2020-01-22 12:58:30 +0000

    3rd stage of GA project

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+    {
+        rank: 'queen',
+        suit: 'hearts',
+        cardImage: 'images/queen-of-hearts.png'
+    },
+    {
+        rank: 'queen',
+        suit: 'diamonds',
+        cardImage: 'images/queen-of-diamonds.png'
+    },
+    {
+        rank: 'king',
+        suit: 'hearts',
+        cardImage: 'images/king-of-diamonds.png'
+    },
+    {
+        rank: 'king',
+        suit: 'diamonds',
+        cardImage: 'images/king-of-diamonds.png'
+    }
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -12,8 +34,11 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-    console.log('User flipped ' + cards[cardId]);
-    cardsInPlay.push(cards[cardId])
+    console.log('User flipped ' + cards[cardId].rank);
+    console.log(cards[cardId].cardImage)
+    console.log(cards[cardId].suit)
+
+    cardsInPlay.push(cards[cardId].rank)
 
     checkForMatch();
     
@@ -21,3 +46,4 @@ function flipCard(cardId) {
 
 flipCard(0);
 flipCard(2);
+
